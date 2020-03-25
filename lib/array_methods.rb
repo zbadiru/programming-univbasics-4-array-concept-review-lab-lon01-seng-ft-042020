4d225ca3e7ca14265acde2120e3b8d6753b4a608
Zakari Badiru <zbadiru@live.co.uk>
2020-03-25 08:40:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,11 @@ def find_element_index(array, value_to_find)
   count = 0 
   while count > array.length do 
     if array[count] == value_to_find
-      
+      return count
+    end
+    count += 1
+  end
+  
 end
 
 def find_max_value(array)
