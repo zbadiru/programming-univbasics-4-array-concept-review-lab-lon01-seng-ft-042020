0c6c932cf41daee5c7aa9f1dbd147cd08207ad01
Zakari Badiru <zbadiru@live.co.uk>
2020-03-25 08:34:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   array.length.times do |count|
-    if array(count) == value_to_find
+    if array[count] == value_to_find
     return count
   end
   nil
