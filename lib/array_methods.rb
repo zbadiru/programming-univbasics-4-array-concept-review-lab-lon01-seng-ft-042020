c563f079f93d101ae47108c69c8aba7ec4e96cbd
Zakari Badiru <zbadiru@live.co.uk>
2020-03-25 08:08:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  value = find_max_value.map(&:las)
+  value = find_max_value.map(&:last)
+  
 end
 
 def find_min_value(array)
