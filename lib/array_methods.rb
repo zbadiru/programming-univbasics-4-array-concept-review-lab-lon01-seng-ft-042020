3cf832eba5f41c6aac1a34ef3c924bb9f0e0626e
Zakari Badiru <zbadiru@live.co.uk>
2020-03-25 08:41:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   count = 0 
-  while count > array.length do 
+  while count < array.length do 
     if array[count] == value_to_find
       return count
     end
